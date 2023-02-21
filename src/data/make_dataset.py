ed2cdbc8414f854499553559eaddc768f734ad62
Maxence Frenette <maxence.frenette@gmail.com>
2023-02-20 22:54:00 -0800

    Adapt Casey's code to run the youtube API

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,26 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
+def main():
+    # Set your API credentials
+    credentials = service_account.Credentials.from_service_account_file(
+        'credentials.json',
+        scopes=['https://www.googleapis.com/auth/youtube.readonly'])
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    # Create a YouTube API client
+    youtube = build('youtube', 'v3', credentials=credentials)
 
+    # Make a request to the API to get a sample of 10 videos
+    response = youtube.videos().list(
+        part='snippet',
+        chart='mostPopular',
+        maxResults=10
+    ).execute()
+
+    # Print the video high res thumbnail URL
+    for item in response['items']:
+        print(item['snippet']['thumbnails']['high']['url'])
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
