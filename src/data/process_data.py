798a44cab004cd4ea596b4e71eca663d10e38131
caseyhahn <casey.hahn@berkeley.edu>
2023-04-05 21:34:47 -0700

    update process_data.py

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-def process_data():
+
+def process_data(buckets=4):
     
     # Read CSV file
     yt_data = pd.read_csv('../data/youtube_video_data.csv')
@@ -47,10 +50,25 @@ def process_data():
     yt_data['log_comments'] = np.log(yt_data['comments'])
     yt_data['log_channel_views'] = np.log(yt_data['channel_views'])
     yt_data['log_channel_subscribers'] = np.log(yt_data['channel_subscribers'])
-    yt_data.drop(columns = ['views','hours_published_video','likes','comments','channel_views','channel_subscribers'], inplace=True)
+    
+    
+    #create view bucket features
+    def create_categorical_variable(df, num_col,buckets):
+        # Compute the percentile cutoffs
+        percentile_cutoffs = list(range(0, 101, int(100/buckets)))
+        percentile_values = [np.percentile(df[num_col], i) for i in percentile_cutoffs]
+
+        # Create the categorical variable
+        field_name = 'views_category_' + str(buckets)
+        df[field_name] = pd.cut(df[num_col], bins=percentile_values, labels=percentile_cutoffs[:-1])
+
+        return df
+    
+    yt_data = create_categorical_variable(yt_data,'views',buckets=buckets)
+
+    yt_data.drop(columns = ['log_views','views','hours_published_video','likes','comments','channel_views','channel_subscribers'], inplace=True)
 
     #likes, and comments are all outcome variables rather than predictors. 
-    #drop these features. Likes / dislikes ratio was discussed but was not included in API data file. 
     yt_data.drop(columns= ['log_likes','log_comments'],inplace=True)
 
     yt_data = yt_data.astype({col: float for col in yt_data.select_dtypes(include='int64').columns})
@@ -62,15 +80,13 @@ def process_data():
     #drop channel_views since highly correlated with channel_subscribers
     yt_data.drop(columns=['log_channel_views'], inplace = True)
 
+   
     # Split into training & test sets
-    # 1,274 rows total -- approx 80/20 split
-    train = yt_data[:1000]
-    test = yt_data[1000:]
-
-    X_train = train.drop(['log_views'], axis=1)
-    y_train = train['log_views']
-    X_test = test.drop(['log_views'], axis=1)
-    y_test = test['log_views']
+    y_label = 'views_category_'+str(buckets)
+    X = yt_data.drop(columns=[y_label])
+    y = yt_data[[y_label]]
+   
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     return X_train, y_train,X_test,y_test
 
